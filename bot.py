1a39cbcfaaa238e27af35272be90a96e48982c37
aya <aya.kawabe3@gmail.com>
2016-11-24 01:34:08 +0900

    remove unnecessary part

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from logging import DEBUG, StreamHandler, getLogger
 
 import requests
 
-import doco.client
 import falcon
 
 # logger
@@ -15,7 +14,6 @@ logger.setLevel(DEBUG)
 logger.addHandler(handler)
 
 ENDPOINT_URI = 'https://trialbot-api.line.me/v1/events'
-DOCOMO_API_KEY = os.environ.get('DOCOMO_API_KEY', '')
 
 
 class CallbackResource(object):
@@ -26,10 +24,6 @@ class CallbackResource(object):
         'X-Line-ChannelSecret': os.environ['LINE_CHANNEL_SECRET'],
         'X-Line-Trusted-User-With-ACL': os.environ['LINE_CHANNEL_MID'],
     }
-
-    # docomo
-    user = {'t': 20}  # 20:kansai character
-    docomo_client = doco.client.Client(apikey=DOCOMO_API_KEY, user=user)
 
     def on_post(self, req, resp):
 
@@ -45,17 +39,6 @@ class CallbackResource(object):
 
             logger.debug('msg: {}'.format(msg))
 
-            try:
-                docomo_res = self.docomo_client.send(
-                    utt=msg['content']['text'], apiname='Dialogue')
-
-            except Exception:
-                raise falcon.HTTPError(falcon.HTTP_503,
-                                       'Docomo API Error. ',
-                                       'Could not invoke docomo api.')
-
-            logger.debug('docomo_res: {}'.format(docomo_res))
-
             send_content = {
                 'to': [msg['content']['from']],
                 'toChannel': 1383378250,  # Fixed value
@@ -63,7 +46,7 @@ class CallbackResource(object):
                 'content': {
                     'contentType': 1,
                     'toType': 1,
-                    'text': docomo_res['utt'],
+                    'text': [msg['content']['from']],
                 },
             }
             send_content = json.dumps(send_content)
